63d07d5215467eee71bbfee57e70c2a0298d00a0
zahrank21 <zahrank@gmail.com>
2017-12-14 23:39:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(string)
   if string == string.upcase
-      if string = 'I LOVE YOU GRANDMA!'
+      if string == 'I LOVE YOU GRANDMA!'
         return 'I LOVE YOU TOO PUMPKIN!'
       else
         return 'NO, NOT SINCE 1938!'
@@ -22,5 +22,4 @@ def speak_to_grandma(string)
   elsif string != string.upcase
     return 'HUH?! SPEAK UP, SONNY!'
   end
-
 end
